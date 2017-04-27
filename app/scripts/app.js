e41e1b34e19f4b8c82122a94bb0686d60a5a3f91
Jonathan Trujillo <jtrujillo@gmail.com>
2017-04-27 00:01:59 -0700

    chat2test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,15 @@
+(function() {
+     function config($stateProvider, $locationProvider) {
+        $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
+         });
+         $stateProvider
+         .state('room', {
+             url: '/',
+             controller: 'RoomCtrl as room',
+             templateUrl: '/templates/home.html'
+         })
+
 angular.module('blocChat' , []);
