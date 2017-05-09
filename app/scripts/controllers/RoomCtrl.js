4610a26f4f45ea289d74939b1c99cbe05752ca62
Jonathan Trujillo <jtrujillo@gmail.com>
2017-05-09 00:33:53 -0700

    almostcreatingrooms

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,10 +1,17 @@
 (function() {
-     function RoomCtrl(Room) {
+     function RoomCtrl(Room, $uibModal) {
        // Use the Room service to get all rooms; assign rooms to a property.
        this.rooms = Room.all;
+       this.addRoom = function() {
+         $uibModal.open({
+           templateUrl: '/templates/modal.html',
+           size: 'sm',
+           controller: 'ModalCtrl as modal'
+           });
+       }
      }
 
      angular
          .module('blocChat')
-         .controller('RoomCtrl', ['Room', RoomCtrl]);
+         .controller('HomeCtrl', ['Room', '$uibModal', RoomCtrl]);
  })();
